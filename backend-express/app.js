44ed0e4f1175f0f3f45edfee09e2933db0e6be5c
invi <roflankaa@gmail.com>
2024-12-09 23:50:35 +0100

    configugrations

--- backend-express/app.js
+++ backend-express/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require("cors");
+const corsOptions = {
+    origin: ["http://localhost:5173"],
+}
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(logger('dev'));
-app.use(express.json());
+// app.use(express.json());
 
 module.exports = app;
